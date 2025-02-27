b815945d761a9ec901a2f356ae40124365962c5b
WillHandel <105618933+WillHandel@users.noreply.github.com>
2025-02-26 19:58:03 -0500

    update1

---
+++ githubGetter.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def getGithubInfo(githubUser):
+    repos = requests.get(f"https://api.github.com/users/{githubUser}/repos").json()
+
+    info = []
+    for repo in repos:
+        name = repo["name"]
+        commits = requests.get(f"https://api.github.com/repos/{githubUser}/{name}/commits").json()
+        commitAmnt = len(commits)
+        info.append(f"Repo: {name}, Number of commits: {commitAmnt}")
+    return info
+
+
+    pass
+
+
+print(getGithubInfo("WillHandel"))
